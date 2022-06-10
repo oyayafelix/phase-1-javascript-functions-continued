af22d29baac5a598c4694470e46c26163ef21386
oyayafelix <oyayafelix@gmail.com>
2022-06-10 20:36:58 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+ }
+ 
+ 
+ function mondayWork (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+
+ }
+ 
+ 
+ function wrapAdjective(visualFlair = "||"){
+   return function (result = "a dedicated programmer"){
+      return `You are ${visualFlair}${result}${visualFlair}!`;
+    }
+  } 
+ 
+ 
